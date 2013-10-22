352bcdcf8ee32749080052cf72466d6c00abe0b6
mijael feldman <mijafeldman@hotmail.com>
2013-10-22 19:43:19 -0300

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,8 @@
+class PagesController < ApplicationController
+	def home
+	end
+	def programa
+	end
+	def mentores
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bombaweb::Application.config.secret_token = 'cb36a98cb365972e945f4aee7e2e83b6552278a3a268dadb75dc31a66445bd176105665d8ad84333eeab286f6a1e6ed947c6af470a3c088a6ac92649e183918d'
